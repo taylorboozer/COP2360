6bcfe0d2be7938d143e43fbc55a0d18684e99b4d
taylorboozer <groovypisces@gmail.com>
2024-09-28 06:10:12 -0400

    Add files via upload

---
+++ testing.cs
@@ -0,0 +1,8 @@
+cd path/to/your/project  # Change this to your project directory
+echo "# COP2360" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/taylorboozer/COP2360.git
+git push -u origin main
