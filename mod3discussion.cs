e0552e3d51631778b7e2867699666def9d571a13
taylorboozer <groovypisces@gmail.com>
2024-10-13 14:07:48 -0400

    Add files via upload

---
+++ mod3discussion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Octopus
+{
+    // Readonly fields
+    public readonly string Name;
+    public readonly int Legs = 8;
+
+    // Regular field
+    public int Age = 10;
+
+    // Constructor
+    public Octopus(string name)
+    {
+        Name = name;
+    }
+
+    // Method to display octopus information
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name: {Name}, Age: {Age}, Legs: {Legs}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Create an Octopus object and display its info
+        Octopus octopus = new Octopus("Jack");
+        octopus.DisplayInfo();
+    }
+}
